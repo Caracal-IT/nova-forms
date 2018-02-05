17d5cbbb12b14bc61d9c2be1de96afe8f368b5a1
Ettiene Mare <ettienemare@gmail.com>
2018-02-05 23:46:53 +0200

    Add Angular workflow

--- src/app/modules/dynamic-form/services/dynamic-form.service.ts
+++ src/app/modules/dynamic-form/services/dynamic-form.service.ts
@@ -4,12 +4,12 @@ import {ComponentService} from "./component.service";
 @Injectable()
 export class DynamicFormService {
     constructor(private componetService: ComponentService){ }
-    
+
     createComponent(container: any, config: any, model: any, wf: any) {
         const dynamicForm = this.componetService.resolve("dynamicForm");
         container.clear();
-        
-        var component = container.createComponent(dynamicForm);
+
+        const component = container.createComponent(dynamicForm);
         component.instance.config = config;
         component.instance.model = model;
         component.instance.wf = wf;
